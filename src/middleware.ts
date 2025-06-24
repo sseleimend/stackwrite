d11232a1a1b6e107f49a4325e1f35c636a4fdc9c
Victor Seleimend <sseleimend@gmail.com>
2025-06-24 12:11:55 -0300

    Add middleware to initialize database and storage on request

---
+++ src/middleware.ts
@@ -0,0 +1,13 @@
+import { MiddlewareConfig, NextRequest, NextResponse } from "next/server";
+import getOrCreateDB from "./models/server/dbSetup";
+import getOrCreateStorage from "./models/server/storageSetup";
+
+export async function middleware(request: NextRequest) {
+  await Promise.all([getOrCreateDB(), getOrCreateStorage()]);
+
+  return NextResponse.next();
+}
+
+export const config: MiddlewareConfig = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
